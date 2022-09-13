87f965195d81c5d22b2cab193421b5f1f7da0648
Matt <96631237+Matt-Willis-20@users.noreply.github.com>
2022-09-13 10:22:00 +0100

    app.css removed - moved to index.css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom'
-import { Link } from 'react-router-dom';
-import './app.css';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Blog from './pages/Blog';
 import About from './pages/About';
 import Contact from './pages/Contact';
-
 
 import FloatToTop from './components/floatToTop/FloatToTop';
 import Navbar from './components/navbar/Navbar';
