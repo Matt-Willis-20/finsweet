9028ce9c5e2e78a1732c5a3337fc4dccfcf5cfe6
Matt <96631237+Matt-Willis-20@users.noreply.github.com>
2022-09-21 14:42:29 +0100

    docs: update the blogsContent list with blog id

--- src/helpers/blogsContent.js
+++ src/helpers/blogsContent.js
@@ -1,5 +1,6 @@
 export const blogsContent = [
     {
+        id: 1,
         category: "startup",
         title: "How to build rapport with your web design clients",
         src: "blog01",
@@ -7,6 +8,7 @@ export const blogsContent = [
         intro: "Duis aute dalor in reprehendrition volutpe veili esses cillm dalore su figure nulla pariator. Execptur sint occat cupidator non proident."
     },
     {
+        id: 2,
         category: "startup",
         title: "Top 10 Hackathon Ideas 2018",
         src: "blog02",
@@ -14,6 +16,7 @@ export const blogsContent = [
         intro: "Duis aute dalor in reprehendrition volutpe veili esses cillm dalore su figure nulla pariator. Execptur sint occat cupidator non proident."
     },
     {
+        id: 3,
         category: "economy",
         src: "blog03",
         title: "6 Steps to Closed-Loop Communications",
@@ -21,6 +24,7 @@ export const blogsContent = [
         intro: "Duis aute dalor in reprehendrition volutpe veili esses cillm dalore su figure nulla pariator. Execptur sint occat cupidator non proident."
     },
     {
+        id: 4,
         category: "economy",
         src: "blog04",
         title: "The Mad Men’s Guide to Job Titles at an Advertising Agency",
@@ -28,6 +32,7 @@ export const blogsContent = [
         intro: "Duis aute dalor in reprehendrition volutpe veili esses cillm dalore su figure nulla pariator. Execptur sint occat cupidator non proident."
     },
     {
+        id: 5,
         category: "technology",
         src: "blog05",
         title: "How to build rapport with your web design clients",
@@ -35,6 +40,7 @@ export const blogsContent = [
         intro: "Duis aute dalor in reprehendrition volutpe veili esses cillm dalore su figure nulla pariator. Execptur sint occat cupidator non proident."
     },
     {
+        id: 6, 
         category: "technology",
         src: "blog06",
         title: "Microsoft Teams Vs Slack: Extended With Quick Infographic",
@@ -42,6 +48,7 @@ export const blogsContent = [
         intro: "Duis aute dalor in reprehendrition volutpe veili esses cillm dalore su figure nulla pariator. Execptur sint occat cupidator non proident."
     },
     {
+        id: 7,
         category: "business",
         src: "blog07",
         title: "How to Write an Email Asking for a Referral or Recommendation",
@@ -49,6 +56,7 @@ export const blogsContent = [
         intro: "Duis aute dalor in reprehendrition volutpe veili esses cillm dalore su figure nulla pariator. Execptur sint occat cupidator non proident."
     },
     {
+        id: 8,
         category: "startup",
         src: "blog08",
         title: "How to build rapport with your web design clients",
@@ -56,6 +64,7 @@ export const blogsContent = [
         intro: "Duis aute dalor in reprehendrition volutpe veili esses cillm dalore su figure nulla pariator. Execptur sint occat cupidator non proident."
     },
     {
+        id: 9,
         category: "business",
         src: "blog09",
         title: "How to build rapport with your web design clients",
@@ -63,6 +72,7 @@ export const blogsContent = [
         intro: "Duis aute dalor in reprehendrition volutpe veili esses cillm dalore su figure nulla pariator. Execptur sint occat cupidator non proident."
     },
     {
+        id: 10,
         category: "technology",
         src: "blog10",
         title: "How to Integrate Shopify With WordPress",
@@ -70,6 +80,7 @@ export const blogsContent = [
         intro: "Duis aute dalor in reprehendrition volutpe veili esses cillm dalore su figure nulla pariator. Execptur sint occat cupidator non proident."
     },
     {
+        id: 11,
         category: "business",
         src: "blog11",
         title: "How to build rapport with your web design clients",
@@ -77,6 +88,7 @@ export const blogsContent = [
         intro: "Duis aute dalor in reprehendrition volutpe veili esses cillm dalore su figure nulla pariator. Execptur sint occat cupidator non proident."
     },
     {
+        id: 12,
         category: "business",
         src: "blog12",
         title: "How to build rapport with your web design clients",
@@ -84,6 +96,7 @@ export const blogsContent = [
         intro: "Duis aute dalor in reprehendrition volutpe veili esses cillm dalore su figure nulla pariator. Execptur sint occat cupidator non proident."
     },
     {
+        id: 13, 
         category: "business",
         src: "blog13",
         title: "How to build rapport with your web design clients",
@@ -91,6 +104,7 @@ export const blogsContent = [
         intro: "Duis aute dalor in reprehendrition volutpe veili esses cillm dalore su figure nulla pariator. Execptur sint occat cupidator non proident."
     },
     {
+        id: 14, 
         category: "startup",
         title: "How to build rapport with your web design clients",
         src: "blog01",
@@ -98,6 +112,7 @@ export const blogsContent = [
         intro: "Duis aute dalor in reprehendrition volutpe veili esses cillm dalore su figure nulla pariator. Execptur sint occat cupidator non proident."
     },
     {
+        id: 15,
         category: "startup",
         title: "Top 10 Hackathon Ideas 2018",
         src: "blog02",
@@ -105,6 +120,7 @@ export const blogsContent = [
         intro: "Duis aute dalor in reprehendrition volutpe veili esses cillm dalore su figure nulla pariator. Execptur sint occat cupidator non proident."
     },
     {
+        id: 16,
         category: "economy",
         src: "blog03",
         title: "6 Steps to Closed-Loop Communications",
@@ -112,6 +128,7 @@ export const blogsContent = [
         intro: "Duis aute dalor in reprehendrition volutpe veili esses cillm dalore su figure nulla pariator. Execptur sint occat cupidator non proident."
     },
     {
+        id: 17,
         category: "economy",
         src: "blog04",
         title: "The Mad Men’s Guide to Job Titles at an Advertising Agency",
