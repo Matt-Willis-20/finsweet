48e7b01b0005b1c5ce0e9d5cfc176dc25041652e
Matt <96631237+Matt-Willis-20@users.noreply.github.com>
2022-09-22 10:44:55 +0100

    refactor: update large blog's key to be blog.id
    updated from using the index as the key

--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -6,7 +6,7 @@ const Blogs = ({ blogsList }) => {
     <>
       <div className='blogs-list'>
         {blogsList.map((blog) => (
-          <div className='large-blog-post-container' key={blog-id} category={blog.category}>
+          <div className='large-blog-post-container' key={blog.id} category={blog.category}>
             <img src={require("../../assets/images/" + blog.src + ".jpg")} alt={blog.alt} />
             <div className='large-blog-post-content'>
               <span className='body-text-01'>{blog.category}</span>
