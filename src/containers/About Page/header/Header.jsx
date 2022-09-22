e6192a8fb731f12cc3148724dff10a9ee85314ec
Matt <96631237+Matt-Willis-20@users.noreply.github.com>
2022-09-22 10:32:12 +0100

    refactor: add jsx content for header

--- src/containers/About Page/header/Header.jsx
+++ src/containers/About Page/header/Header.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <section className='section-padding' id="about-page-header">
+      <div className='about-header-container'>
+        <div className='about-header-title'>
+          <p className='body-text-01'>About us</p>
+          <h1>We are a team of content writers who share their learnings</h1>
+        </div>
+        <div className="about-header-text">
+        <p className="body-text-01">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        
+      </div>
+    </section>
   )
 }
 
