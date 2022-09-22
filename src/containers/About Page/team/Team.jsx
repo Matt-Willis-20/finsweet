b6415fe7e012f3cd363478382ae0b797ad6c65c4
Matt <96631237+Matt-Willis-20@users.noreply.github.com>
2022-09-22 12:19:52 +0100

    first commit - initial jsx added

---
+++ src/containers/About Page/team/Team.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './team.css';
+
+const Team = () => {
+  return (
+    <section className='section-padding about-page-team'>
+        <div className='about-page-team-container'>
+            <div className='about-page-team-content'>
+                <h2>Our team of creatives</h2>
+                <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</h4>
+                <p className='body-text-01'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat.</p>
+            </div>
+            <div className='about-page-team-image'></div>
+        </div>
+        <div className='about-page-why-container'>
+            <div className='about-page-why-image'></div>
+            <div className='about-page-why-content'>
+                <h2>Why we started this Blog</h2>
+                <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</h4>
+                <p className='body-text-01'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat.</p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Team
