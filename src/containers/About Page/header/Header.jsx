d77bf979320625e0cc62eb419f3da3c19d937778
Matt <96631237+Matt-Willis-20@users.noreply.github.com>
2022-09-22 14:58:50 +0100

    refactor: add correct code indentation

--- src/containers/About Page/header/Header.jsx
+++ src/containers/About Page/header/Header.jsx
@@ -10,9 +10,8 @@ const Header = () => {
           <h1>We are a team of content writers who share their learnings</h1>
         </div>
         <div className="about-header-text">
-        <p className="body-text-01">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <p className="body-text-01">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         </div>
-        
       </div>
     </section>
   )
