9154d9eae2d81477243cf93b680dcb5a322d91e3
Matt <96631237+Matt-Willis-20@users.noreply.github.com>
2022-09-23 14:24:40 +0100

    refactor: combine container imports to a single line

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,22 @@
 import React, { useEffect } from 'react';
-import Header from '../containers/Home Page/header/Header';
-import Blog from '../containers/Home Page/blogs/Blogs';
-import About from '../containers/Home Page/about/About';
-import Category from '../containers/category/Category';
-import Why from '../containers/Home Page/why/Why';
-import Authors from '../containers/Home Page/authors/Authors';
-import Featured from '../containers/Home Page/featured/Featured';
-import Testimonials from '../containers/Home Page/testimonials/Testimonials';
-import Join from '../containers/join/Join';
+import { Join, Category, Mission, Why, Testimonials, Featured, Blog, Header, HomePageAuthors } from '../containers/index';
 
 
 const Home = () => {
-  
+
   useEffect(() => {
     //scroll to top on page load
-    window.scrollTo({top: 0, behaviour:'smooth'});
+    window.scrollTo({ top: 0, behaviour: 'smooth' });
   }, []);
 
   return (
     <>
       <Header />
       <Blog />
-      <About />
+      <Mission />
       <Category />
       <Why />
-      <Authors />
+      <HomePageAuthors />
       <Featured />
       <Testimonials />
       <Join />
