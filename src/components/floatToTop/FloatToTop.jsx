37d0e867d9673a63c7b666df570e8113b48e5f1b
Matt <96631237+Matt-Willis-20@users.noreply.github.com>
2022-09-23 12:41:17 +0100

    refactor: reformat code layout and spacing only

--- src/components/floatToTop/FloatToTop.jsx
+++ src/components/floatToTop/FloatToTop.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import './floatToTop.css';
-import {BsArrowUp} from 'react-icons/bs';
+import { BsArrowUp } from 'react-icons/bs';
 
 const FloatToTop = () => {
-   // The back-to-top button is hidden at the beginning
+  // The back-to-top button is hidden at the beginning
   const [showButton, setShowButton] = useState(false);
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
-        (window.pageYOffset > 400) ? setShowButton(true) : setShowButton(false)
+      (window.pageYOffset > 400) ? setShowButton(true) : setShowButton(false)
     });
   }, []);
 
@@ -17,16 +17,16 @@ const FloatToTop = () => {
       top: 0,
       behavior: 'smooth'
     });
-  }; 
+  };
 
   return (
     <>
-    {showButton && (
+      {showButton && (
         <button onClick={scrollToTop} id="float-to-top" className='fade-in-fwd'>
-            <BsArrowUp />
-            <p>TOP</p>
+          <BsArrowUp />
+          <p>TOP</p>
         </button>
-    )}
+      )}
     </>
   )
 }
