580c281aa00787d42a27da7db2a4f1a46cbe260a
Matt <96631237+Matt-Willis-20@users.noreply.github.com>
2022-09-23 23:16:43 +0100

    feat: add category context
    removed requirement for using element.innerText

--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -1,34 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import Blogs from '../components/blogs/Blogs';
 import { CategoryHeader, Sidebar } from '../containers/index';
 import { blogsContent } from '../helpers/blogsContent';
+
+import { CategoryData } from '../components/CategoryContext';
+
 import './categoryPage.css'
 
 const CategoryPage = () => {
+  const [categoryData, setCategoryData] = useContext(CategoryData);
+  const [blogsList] = useState(blogsContent);
+  const [categoryBlogList, setCategoryBlogList] = useState(blogsContent.filter(blog => blog.category === categoryData));
 
   useEffect(() => {
     //scroll to top on page load
     window.scrollTo({ top: 0, behaviour: 'smooth' });
   }, []);
 
-  const [blogsList] = useState(blogsContent);
-  const [categoryBlogList, setCategoryBlogList] = useState(blogsContent);
-
-  const pageTitle = document.getElementById('category-page-title');
-  const pageCategory = document.getElementById('category-page-category');
-
   const filterBlogs = (currentCategory) => {
+    setCategoryData(currentCategory);
     if (currentCategory === "All Posts") {
       setCategoryBlogList(blogsContent);
-      pageTitle.innerText = (currentCategory);
-      pageCategory.innerText = (currentCategory);
     } else {
       const newBlogsList = blogsList.filter((newCategory) => {
         return newCategory.category === currentCategory;
       });
       setCategoryBlogList(newBlogsList);
-      pageTitle.innerText = (currentCategory);
-      pageCategory.innerText = (currentCategory);
     }
   };
 
