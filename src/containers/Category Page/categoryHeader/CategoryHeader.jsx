bdcf126e53e155f3c433299f102f0416414f5fc2
Matt <96631237+Matt-Willis-20@users.noreply.github.com>
2022-09-23 12:54:05 +0100

    refactor: update component name to CategoryHeader
    update component name from Header to CategoryHeader.jsx

---
+++ src/containers/Category Page/categoryHeader/CategoryHeader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './categoryHeader.css';
+
+export const CategoryHeader = () => {
+  return (
+    <header id="category-page-header" className='section-padding'>
+        <p className='display-text' id="category-page-title">Business</p>
+        <p className='body-text-01'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+        <p>blog - <span id="category-page-category">business</span></p>
+    </header>
+  )
+}
+
+export default CategoryHeader
