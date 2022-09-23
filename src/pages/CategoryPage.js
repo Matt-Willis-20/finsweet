76c577b774c2ee9f9ec898abe3b5f856e06ca72e
Matt <96631237+Matt-Willis-20@users.noreply.github.com>
2022-09-23 14:39:04 +0100

    refactor: combine container imports to a single line

--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -1,40 +1,43 @@
 import React, { useState, useEffect } from 'react';
-import Header from '../containers/Category Page/header/Header';
 import Blogs from '../components/blogs/Blogs';
-import Sidebar from '../containers/Category Page/sidebar/Sidebar';
+import { CategoryHeader, Sidebar } from '../containers/index';
 import { blogsContent } from '../helpers/blogsContent';
-
 import './categoryPage.css'
 
 const CategoryPage = () => {
 
   useEffect(() => {
     //scroll to top on page load
-    window.scrollTo({top: 0, behaviour:'smooth'});
+    window.scrollTo({ top: 0, behaviour: 'smooth' });
   }, []);
 
-    const [blogsList] = useState(blogsContent);
-    const [categoryBlogList, setCategoryBlogList] = useState(blogsContent);
+  const [blogsList] = useState(blogsContent);
+  const [categoryBlogList, setCategoryBlogList] = useState(blogsContent);
 
-    const pageTitle = document.getElementById('category-page-title');
-    const pageCategory = document.getElementById('category-page-category');
+  const pageTitle = document.getElementById('category-page-title');
+  const pageCategory = document.getElementById('category-page-category');
 
-
-    const filterBlogs = (currentCategory) => {
+  const filterBlogs = (currentCategory) => {
+    if (currentCategory === "All Posts") {
+      setCategoryBlogList(blogsContent);
+      pageTitle.innerText = (currentCategory);
+      pageCategory.innerText = (currentCategory);
+    } else {
       const newBlogsList = blogsList.filter((newCategory) => {
         return newCategory.category === currentCategory;
       });
       setCategoryBlogList(newBlogsList);
       pageTitle.innerText = (currentCategory);
       pageCategory.innerText = (currentCategory);
-    };
+    }
+  };
 
   return (
     <>
-    <Header />
+      <CategoryHeader />
       <div className='category-page-container section-padding'>
-          <Blogs blogsList={categoryBlogList} />
-          <Sidebar filterBlogs={filterBlogs}  />
+        <Blogs blogsList={categoryBlogList} />
+        <Sidebar filterBlogs={filterBlogs} />
       </div>
     </>
   )
