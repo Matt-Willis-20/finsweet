d8dca4785fcddba3b2d80a070a40d2982bfa5496
Matt <96631237+Matt-Willis-20@users.noreply.github.com>
2022-09-23 23:18:02 +0100

    feat: add onClick to setCategoryData

--- src/containers/category/Category.jsx
+++ src/containers/category/Category.jsx
@@ -1,27 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import './category.css';
 import CategorySmall from '../../components/categorySmall/CategorySmall';
-import { FaRocket } from 'react-icons/fa';
-import { FaBuilding } from 'react-icons/fa';
-import { FaChartLine } from 'react-icons/fa';
+import { FaRocket, FaBuilding, FaChartLine } from 'react-icons/fa';
 import { GrTechnology } from 'react-icons/gr';
 
+import { CategoryData } from '../../components/CategoryContext';
+
 const Category = () => {
+  const [categoryData, setCategoryData] = useContext(CategoryData);
+
   return (
     <section className='section-padding'>
       <h2 className='home-category-title'>Choose A Category</h2>
       <div className='home-small-category-container'>
-        <Link to='/categoryPage'>
+        <Link to='/categoryPage' onClick={() => setCategoryData("business")}>
           <CategorySmall icon={<FaBuilding />} category="business" text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit." />
         </Link>
-        <Link to='/categoryPage'>
+        <Link to='/categoryPage' onClick={() => setCategoryData("startup")}>
           <CategorySmall icon={<FaRocket />} category="startup" text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit." />
         </Link>
-        <Link to='/categoryPage'>
+        <Link to='/categoryPage' onClick={() => setCategoryData("economy")}>
           <CategorySmall icon={<FaChartLine />} category="economy" text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit." />
         </Link>
-        <Link to='/categoryPage'>
+        <Link to='/categoryPage' onClick={() => setCategoryData("technology")}>
           <CategorySmall icon={<GrTechnology />} category="technology" text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit." />
         </Link>    
       </div>
