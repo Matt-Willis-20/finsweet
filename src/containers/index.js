33bca38d1554b5f041a0f4f81ec7d6340340ec64
Matt <96631237+Matt-Willis-20@users.noreply.github.com>
2022-09-23 13:11:33 +0100

    refactor: add dditional exports to file

---
+++ src/containers/index.js
@@ -0,0 +1,31 @@
+// Global Containers ----------------------- //
+
+export { default as Join } from './join/Join';
+export { default as Footer} from './footer/Footer';
+export { default as Category } from './category/Category';
+
+
+// Home Page Containers --------------------- //
+
+export { default as Header } from './Home Page/header/Header';
+export { default as Blog } from './Home Page/blogs/Blogs';
+export { default as Mission } from './Home Page/mission/Mission';
+export { default as Why } from './Home Page/why/Why';
+export { default as Testimonials } from './Home Page/testimonials/Testimonials';
+export { default as Featured } from './Home Page/featured/Featured';
+
+
+// Blog Page Containers ------------------- //
+
+
+// About Page Containers ----------------------- //
+
+export { default as AboutHeader } from './About Page/aboutHeader/AboutHeader';
+export { default as Team } from './About Page/team/Team';
+export { default as Purpose } from './About Page/purpose/Purpose';
+
+
+//  Category Page -------------------------- // 
+
+export { default as CategoryHeader } from './Category Page/categoryHeader/CategoryHeader';
+export { default as Sidebar } from './Category Page/sidebar/Sidebar';
