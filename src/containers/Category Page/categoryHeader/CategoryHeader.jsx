b0af8f5d02a9a346a59d9a42c49f03e6d3cf5912
Matt <96631237+Matt-Willis-20@users.noreply.github.com>
2022-09-23 23:22:54 +0100

    feat: add useContext to component
    update jsx content using reference to categoryData

--- src/containers/Category Page/categoryHeader/CategoryHeader.jsx
+++ src/containers/Category Page/categoryHeader/CategoryHeader.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import './categoryHeader.css';
+import { CategoryData } from '../../../components/CategoryContext';
+
 
 export const CategoryHeader = () => {
+  const [categoryData] = useContext(CategoryData);
+
   return (
     <header id="category-page-header" className='section-padding'>
-        <p className='display-text' id="category-page-title">Business</p>
+        <p className='display-text' id="category-page-title">{categoryData}</p>
         <p className='body-text-01'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-        <p>blog - <span id="category-page-category">business</span></p>
+        <p>blog - <span id="category-page-category">{categoryData}</span></p>
     </header>
   )
 }
