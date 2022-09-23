77343253f2532b3bcb9399ccb89eb88e126ba0a7
Matt <96631237+Matt-Willis-20@users.noreply.github.com>
2022-09-23 12:39:33 +0100

    refactor: reformat code layout and spacing only

--- src/components/blogSmall/BlogSmall.jsx
+++ src/components/blogSmall/BlogSmall.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import AuthorTag from '../authorTag/AuthorTag';
-import './blogSmall.css';
-import Blog02 from '../../assets/images/blog02.jpg';
 import { Link } from 'react-router-dom';
+import AuthorTag from '../authorTag/AuthorTag';
+import Blog02 from '../../assets/images/blog02.jpg';
+import './blogSmall.css';
 
-const BlogSmall = ({name, date, title}) => {
+
+const BlogSmall = ({ name, date, title }) => {
   return (
     <Link to='/blogpage'>
       <div className='small-blog-container'>
-          <div className='small-blog-image'>
-            <img src={Blog02} alt="" />
-          </div>
-          <div className='small-blog-content'>
-            <AuthorTag name={name} date={date} />
-            <h4>{title}</h4>
-          </div>
+        <div className='small-blog-image'>
+          <img src={Blog02} alt="" />
+        </div>
+        <div className='small-blog-content'>
+          <AuthorTag name={name} date={date} />
+          <h4>{title}</h4>
+        </div>
       </div>
     </Link>
   )
