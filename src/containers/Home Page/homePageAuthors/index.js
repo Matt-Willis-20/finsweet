82a5d7e49c6c00d9dd51a93540884dad08bf7d44
Matt <96631237+Matt-Willis-20@users.noreply.github.com>
2022-09-23 14:34:48 +0100

    feat: add helper file to import images

---
+++ src/containers/Home Page/homePageAuthors/index.js
@@ -0,0 +1,6 @@
+import Author1 from '../../../assets/images/author1.jpg';
+import Author2 from '../../../assets/images/author2.jpg';
+import Author3 from '../../../assets/images/author3.jpg';
+import Author4 from '../../../assets/images/author4.jpg';
+
+export { Author1, Author2, Author3, Author4};
