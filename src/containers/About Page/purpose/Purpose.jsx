ce53dcbb8a26bcc1c5342a05c4772a6f71ba727f
Matt <96631237+Matt-Willis-20@users.noreply.github.com>
2022-09-23 12:47:30 +0100

    refactor: reformat code layout and spacing only

--- src/containers/About Page/purpose/Purpose.jsx
+++ src/containers/About Page/purpose/Purpose.jsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import './purpose.css'
+import React from 'react';
+import './purpose.css';
 
 const Purpose = () => {
-  return (
-    <section className='section-padding about-page-purpose-container'>
-        <div className='about-page-purpose-image'>
-            <div className='about-page-colorstrip'>
-                <div className='yellow strip'></div>
-                <div className='purple strip'></div>
+    return (
+        <section className='section-padding about-page-purpose-container'>
+            <div className='about-page-purpose-image'>
+                <div className='about-page-colorstrip'>
+                    <div className='yellow strip'></div>
+                    <div className='purple strip'></div>
+                </div>
             </div>
-        </div>
-        <div className='about-page-purpose-content'>
-            <div className='about-page-mission'>
-                <span>our mission</span>
-                <h3>Creating valuable content for creatives all around the world</h3>
-                <p className='body-text-01'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus.</p>
+            <div className='about-page-purpose-content'>
+                <div className='about-page-mission'>
+                    <span>our mission</span>
+                    <h3>Creating valuable content for creatives all around the world</h3>
+                    <p className='body-text-01'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus.</p>
+                </div>
+                <div className='about-page-vision'>
+                    <span>our vision</span>
+                    <h3>A platform that empowers individuals to improve</h3>
+                    <p className='body-text-01'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus.</p>
+                </div>
             </div>
-            <div className='about-page-vision'>
-                <span>our vision</span>
-                <h3>A platform that empowers individuals to improve</h3>
-                <p className='body-text-01'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus.</p>
-            </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default Purpose
