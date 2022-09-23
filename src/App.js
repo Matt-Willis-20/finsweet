28845a98a3a3448168458704c743a02c0b7aaa80
Matt <96631237+Matt-Willis-20@users.noreply.github.com>
2022-09-23 23:19:38 +0100

    feat: add Category Context wrapper

--- src/App.js
+++ src/App.js
@@ -2,31 +2,34 @@ import React, { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Navbar, FloatToTop } from './components/index';
 import Footer from './containers/footer/Footer';
+import CategoryContext from './components/CategoryContext';
 
 const Home = lazy(() => import('./pages/Home'));
-const Blog = lazy(() => import ('./pages/Blog'));
+const Blog = lazy(() => import('./pages/Blog'));
 const Contact = lazy(() => import('./pages/Contact'));
-const About = lazy(() => import ('./pages/About'));
+const About = lazy(() => import('./pages/About'));
 const BlogPage = lazy(() => import('./pages/BlogPage'));
-const CategoryPage = lazy(() => import ('./pages/CategoryPage'));
+const CategoryPage = lazy(() => import('./pages/CategoryPage'));
+
 
 const App = () => {
+
   return (
-    <>
-    <Navbar />
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
+    <CategoryContext>
+      <Navbar />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/blogpage" element={<BlogPage />} />
           <Route path="/categoryPage" element={<CategoryPage />} />
-      </Routes>
-    </Suspense>
-    <Footer />
-    <FloatToTop />
-    </>
+        </Routes>
+      </Suspense>
+      <Footer />
+      <FloatToTop />
+    </CategoryContext>
   )
 }
 
