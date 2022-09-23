42e3a546a3dd794ad9332fd7abac4773a731b11d
Matt <96631237+Matt-Willis-20@users.noreply.github.com>
2022-09-23 12:46:17 +0100

    refactor: update component name to AboutHeader
    update component name from Header to AboutHeader.jsx

---
+++ src/containers/About Page/aboutHeader/AboutHeader.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './aboutHeader.css';
+
+const AboutHeader = () => {
+  return (
+    <section className='section-padding' id="about-page-header">
+      <div className='about-header-container'>
+        <div className='about-header-title'>
+          <p className='body-text-01'>About us</p>
+          <h1>We are a team of content writers who share their learnings</h1>
+        </div>
+        <div className="about-header-text">
+          <p className="body-text-01">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AboutHeader
